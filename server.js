7f9874e544852e1deb499611d8732d25eb1511cf
cousseaualban <cousseaualban@gmail.com>
2026-05-12 12:28:27 +0200

    Dockerfile

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 
 const app = express();
-const PORT = 3000;
 
-// route principale
-app.get('/', (req, res) => {
-  res.send('Hello Express 🚀');
+// ⚡ obligatoire en microservices
+const PORT = process.env.PORT || 8001;
+
+app.get('/health', (req, res) => {
+  res.send('Login service OK');
 });
 
-// démarrage serveur
 app.listen(PORT, () => {
-  console.log(`Serveur lancé sur http://localhost:${PORT}`);
+  console.log(`Login running on ${PORT}`);
 });
